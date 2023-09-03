7a2742110781e35751e5d0ec6a2e5c8b3814f9d0
Carlos Diaz <bycdiaz@icloud.com>
2023-09-03 13:14:42 -0700

    Consolidates edit logic.

--- src/components/EditSection.jsx
+++ src/components/EditSection.jsx
@@ -30,84 +30,26 @@ function EditSection(props) {
   }
 
   function getCurrentFieldValue(label) {
-    const state = getCurrentFormState(props.currentTab);
-
-    if (state === undefined) {
-      return '';
-    }
-
-    if (props.currentTab === 'personal-details') {
-      return (state)[label].toString() || '';
-    }
-
-    if (props.currentTab === 'education') {
-      return (state)[label].toString() || '';
-    }
-
-    if (props.currentTab === 'work-experience') {
-      return (state)[label].toString() || '';
-    }
-
-    return '';
+    return getCurrentFormState(props.currentTab)[label].toString() || '';
   }
 
   function getCurrentFormState(currentTab) {
-    if (currentTab === 'personal-details') {
-      return props.currentFormState[currentTab];
-    } else if (currentTab === 'education') {
-      return props.currentFormState[currentTab];
-    } else if (currentTab === 'work-experience') {
-      return props.currentFormState[currentTab];
-    }
-
+    return props.currentFormState[currentTab];
   };
 
   function getCurrentSetStateFunction() {
-    const currentTab = props.currentTab;
-
-    if (currentTab === 'personal-details') {
-      return props.currentSetStateFunction['personal-details'];
-    } else if (currentTab === 'education') {
-      return props.currentSetStateFunction['education'];
-    } else if (currentTab === 'work-experience') {
-      return props.currentSetStateFunction['work-experience'];
-    }
+    return props.currentSetStateFunction[props.currentTab];
   }
 
   function handleOnChange(updatedValue, label) {
-    const currentTab = props.currentTab;
-    if (currentTab === 'education') {
-      const currentFormState = getCurrentFormState(currentTab);
-      const currentSetStateFunction = getCurrentSetStateFunction();
+    const currentFormState = getCurrentFormState(props.currentTab);
+    const currentSetStateFunction = getCurrentSetStateFunction();
+    const newFormState = {
+      ...currentFormState,
+      [label]: updatedValue
+    };
 
-      const newFormState = {
-        ...currentFormState,
-        [label]: updatedValue
-      };
-
-      currentSetStateFunction(newFormState);
-    } else if (currentTab === 'personal-details') {
-      const currentFormState = getCurrentFormState(currentTab);
-      const currentSetStateFunction = getCurrentSetStateFunction();
-
-      const newFormState = {
-        ...currentFormState,
-        [label]: updatedValue
-      };
-
-      currentSetStateFunction(newFormState);
-    } else if (currentTab === 'work-experience') {
-      const currentFormState = getCurrentFormState(currentTab);
-      const currentSetStateFunction = getCurrentSetStateFunction();
-
-      const newFormState = {
-        ...currentFormState,
-        [label]: updatedValue
-      };
-
-      currentSetStateFunction(newFormState);
-    }
-
+    currentSetStateFunction(newFormState);
   }
 }
 
